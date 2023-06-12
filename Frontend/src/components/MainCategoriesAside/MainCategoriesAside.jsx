4a85aed5f4f27947f3f1213ad7082e87421195b6
Sergii <wheelyhog@gmail.com>
2023-06-12 21:49:24 +0200

    add pagination

--- Frontend/src/components/MainCategoriesAside/MainCategoriesAside.jsx
+++ Frontend/src/components/MainCategoriesAside/MainCategoriesAside.jsx
@@ -4,8 +4,8 @@ import s from './MainCategoriesAside.module.css'
 export default function MainCategoriesAside({ categories }) {
   return (
     <div className={s.aside_wrapper}>
-      {categories.map(elem =>
-        <div className={s.category_item}>{elem.title}</div>
+      {categories.map((elem, index) =>
+        <div className={s.category_item} key={index}>{elem.title}</div>
       )}
     </div>
   )
