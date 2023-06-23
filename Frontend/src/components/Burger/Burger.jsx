92b562fd3e1349540b2d736db0854a6039eee631
Slava <v.zhevaga@gmail.com>
2023-06-23 12:45:47 +0200

    feat(active)burger

--- Frontend/src/components/Burger/Burger.jsx
+++ Frontend/src/components/Burger/Burger.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react'
+import { AiOutlineClose } from 'react-icons/ai';
 import s from './Burger.module.css'
 
 export default function Burger() {
+	const [burgerActive, setBurgerActive] = useState(false);
+
 	return (
-		<div className={s.burger_menu} >
+		<div
+			className={`${s.burger_menu} ${s[burgerActive ? 'active' : ''] || ''}`}
+			onClick={() => setBurgerActive(!burgerActive)}>
 			<span></span>
 			<span></span>
 			<span></span>
+			<AiOutlineClose/>
 		</div>
 	)
 }
