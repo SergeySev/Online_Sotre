74ced98771a3a74cf62ac885c4ee9d7e2061e634
Sergii <wheelyhog@gmail.com>
2023-06-23 21:10:09 +0200

    add adaptive to main categories gallery on homepage

--- Frontend/src/components/CategoryItemGallery/CategoryItemGallery.jsx
+++ Frontend/src/components/CategoryItemGallery/CategoryItemGallery.jsx
@@ -8,14 +8,61 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetch_sub_categories_by_main } from '../../requests/requests';
 import ProductItem from '../ProductItem/ProductItem';
 import './slick_styles.css'
+import prevArrow from '../../media/prevArrow.png'
+import nextArrow from '../../media/nextArrow.png'
 
 export default function CategoryItemGallery({ id, title }) {
-  const settings = {
+
+  const CustomPrevArrow = (props) => {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, width: '40px', height: '40px', backgroundImage: `url(${prevArrow})` }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  const CustomNextArrow = (props) => {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, width: '40px', height: '40px', backgroundImage: `url(${nextArrow})` }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  let settings = {
     dots: false,
     infinite: true,
     speed: 1000,
     slidesToShow: 2,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
+    responsive: [
+      {
+        breakpoint: 1420,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 1120,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 790,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
   }
 
   const dispatch = useDispatch();
