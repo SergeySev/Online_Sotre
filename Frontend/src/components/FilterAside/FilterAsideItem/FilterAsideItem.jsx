2b2c73912d7184ec3c0de0f491b16d5ac4ee4c46
Sergii <wheelyhog@gmail.com>
2023-07-03 12:24:52 +0200

    fix filter checkboxes

--- Frontend/src/components/FilterAside/FilterAsideItem/FilterAsideItem.jsx
+++ Frontend/src/components/FilterAside/FilterAsideItem/FilterAsideItem.jsx
@@ -5,13 +5,20 @@ export default function FilterAsideItem({ title }) {
   const temp_list = ['item1', 'item2', 'item3', 'item4']
   const [activeFilter, setActiveFilter] = useState(false)
 
+  const changeActive = () => {
+    setActiveFilter(!activeFilter)
+  }
+
   return (
-    <li className={!activeFilter ? s.filter_item : s.filter_item_active} onClick={() => setActiveFilter(!activeFilter)}>{title}
+    <li>
+      <div className={!activeFilter ? s.filter_item : s.filter_item_active} onClick={changeActive}>{title}</div>
       <ul className={s.filter_item_list}>
         {temp_list.map((elem, index) => {
           return <form key={index}>
-            <input type={'checkbox'} name={elem} className={s.filter_checkbox} />
-            <label>{elem}</label>
+            <label>{elem}
+              <input type={'checkbox'} name={elem} className={s.filter_checkbox} />
+              <span class={s.checkmark}></span>
+            </label>
           </form>
         })}
       </ul>
