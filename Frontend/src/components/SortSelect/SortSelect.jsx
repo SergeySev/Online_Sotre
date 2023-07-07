9b2482f3d6378e3f08c3dadf6cfaefaeaccd7ae1
Slava <v.zhevaga@gmail.com>
2023-07-07 21:25:47 +0200

    feat(style)select

--- Frontend/src/components/SortSelect/SortSelect.jsx
+++ Frontend/src/components/SortSelect/SortSelect.jsx
@@ -1,19 +1,28 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { sort_ascending_price, sort_by_hits, sort_by_new, sort_by_promo, sort_descending_price, sort_fromA, sort_fromZ } from '../../store/reducers/favoriteSlice';
 import s from './SortSelect.module.css'
 
 export function SortSelect({ content }) {
+
+	const [active, setActive] = useState(false)
 	const refSelect = useRef();
 
 	useEffect(() => {
-		refSelect.current.value = 'new';
+		// refSelect.current = 'new';
+		refSelect.value = 'New first';
 	}, [content]);
 
 	const dispatch = useDispatch();
 
-	const handleSelect = (e) => {
-		switch (e.target.value) {
+	const toogleActive = (e) => {
+		setActive(!active)
+
+	}
+	const handleSelectItem = (e) => {
+		setActive(!active)
+		refSelect.value = e.target.innerText;
+		switch (e.target.dataset.value) {
 			case 'new':
 				dispatch(sort_by_new());
 				break;
@@ -40,23 +49,25 @@ export function SortSelect({ content }) {
 		}
 	}
 	return (
-		<>
-			<p>Sorted:</p>
-			<div className={s.select_wrapper}>
-				<select
-					className={s.select}
-					onChange={handleSelect}
+		<div className={s.form_select}>
+			<label className={s.select_label}>
+				Sorted:
+				<button
+					className={s.select_button}
 					ref={refSelect}
-				>
-					<option value="new" >New first</option>
-					<option value="hits">Popular first</option>
-					<option value="promo">Discount first</option>
-					<option value="asc">Ascending price</option>
-					<option value="desc">Descending price</option>
-					<option value="fromA">From A to Z</option>
-					<option value="fromZ">From Z to A</option>
-				</select>
-			</div>
-		</>
+					onClick={toogleActive}
+				>{refSelect.value}</button>
+			</label>
+			<ul className={`${s.dropdown_list} ${s[active ? 'active' : ""]} || ""}`}>
+				<li className={s.dropdown_item} onClick={handleSelectItem} data-value='new'>New first</li>
+				<li className={s.dropdown_item} onClick={handleSelectItem} data-value='hits'>Popular first</li>
+				<li className={s.dropdown_item} onClick={handleSelectItem} data-value='promo'>Discount first</li>
+				<li className={s.dropdown_item} onClick={handleSelectItem} data-value='asc'>Ascending price</li>
+				<li className={s.dropdown_item} onClick={handleSelectItem} data-value='desc'>Descending price</li>
+				<li className={s.dropdown_item} onClick={handleSelectItem} data-value='fromA'>From A to Z</li>
+				<li className={s.dropdown_item} onClick={handleSelectItem} data-value='fromZ'>From Z to A</li>
+			</ul>
+			{/* <input className={s.select_input} /> */}
+		</div >
 	)
 }
