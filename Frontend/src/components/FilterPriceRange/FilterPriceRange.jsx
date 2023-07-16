54503fe366f1a5d51abf6cdaa54bc2c83dfa152c
Slava <v.zhevaga@gmail.com>
2023-07-17 00:41:22 +0200

    fix(adaptive)checkout

--- Frontend/src/components/FilterPriceRange/FilterPriceRange.jsx
+++ Frontend/src/components/FilterPriceRange/FilterPriceRange.jsx
@@ -2,24 +2,24 @@ import { Box, Slider } from '@mui/material'
 import React, { useState } from 'react'
 
 export default function FilterPriceRange({ max, min, setRequestBody }) {
-  // console.log(typeof (min), typeof (max));
+	// console.log(typeof (min), typeof (max));
 
-  const [value, setValue] = useState([100, 500]);
+	const [value, setValue] = useState([100, 500]);
 
-  const sliderChange = (event, newValue) => {
-    setValue(newValue);
-    setRequestBody('Price', newValue)
-  }
+	const sliderChange = (event, newValue) => {
+		setValue(newValue);
+		setRequestBody('Price', newValue)
+	}
 
-  return (
-    <Box sx={{ width: 200 }}>
-      <Slider
-        value={value}
-        onChange={sliderChange}
-        valueLabelDisplay="auto"
-        min={min} max={max}
-      // color='#000'
-      />
-    </Box>
-  )
+	return (
+		<Box sx={{ width: 200 }}>
+			<Slider
+				value={value}
+				onChange={sliderChange}
+				valueLabelDisplay="auto"
+				min={min} max={max}
+			// color='#000'
+			/>
+		</Box>
+	)
 }
