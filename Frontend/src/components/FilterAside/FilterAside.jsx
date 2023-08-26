200ef7e0e5cfc5e4142180f71c8de6d167a82c4a
Wheelyhog <wheelyhog@gmail.com>
2023-08-26 10:07:46 +0200

    add aside menu in BrandItemPage

--- Frontend/src/components/FilterAside/FilterAside.jsx
+++ Frontend/src/components/FilterAside/FilterAside.jsx
@@ -4,15 +4,13 @@ import { fetch_filter_data, fetch_filtered_subcategory_products } from '../../re
 import s from './FilterAside.module.css'
 import { FilterAsideElem } from './FilterAsideElem/FilterAsideElem';
 
-export function FilterAside({ subcategory_title }) {
+export function FilterAside({ subcategory_title, location }) {
 	const [request_url, setRequest_url] = useState('')
-	// const [request_url, setRequest_url] = useState('http://localhost:8080/api/v1/product/byFilter?page=1&size=30&subCategoryTitle=')
 
 	const dispatch = useDispatch()
 
 	useEffect(() => dispatch(fetch_filter_data()), [dispatch])
 	const filter_data = useSelector(store => store.filter_data)
-	// const requestBody = useSelector(state => state.requestBody)
 
 	useEffect(() => {
 		if (request_url !== 'http://localhost:8080/api/v1/product/byFilter?page=1&size=30&subCategoryTitle=') {
@@ -29,7 +27,9 @@ export function FilterAside({ subcategory_title }) {
 				if (elem === 'Price') {
 					data = filter_data['Price']
 				} else if (elem === 'Brands') {
-					data = filter_data['Brands']
+					if (!location) {
+						data = filter_data['Brands']
+					}
 				} else if (elem === 'Color') {
 					data = filter_data['Color']
 				} else if (elem === 'Delivery Type') {
@@ -37,13 +37,24 @@ export function FilterAside({ subcategory_title }) {
 				} else if (elem === 'Made Country') {
 					data = filter_data['Made Country']
 				}
-				return <FilterAsideElem
-					key={index}
-					title={elem}
-					data={data}
-					subcategory_title={subcategory_title}
-					setRequest_url={setRequest_url}
-				/>
+				if (!location) {
+					return <FilterAsideElem
+						key={index}
+						title={elem}
+						data={data}
+						subcategory_title={subcategory_title}
+						setRequest_url={setRequest_url}
+					/>
+				} else if (elem !== 'Brands') {
+					return <FilterAsideElem
+						key={index}
+						title={elem}
+						data={data}
+						subcategory_title={subcategory_title}
+						setRequest_url={setRequest_url}
+					/>
+				}
+
 			}
 			)}
 		</ul>
