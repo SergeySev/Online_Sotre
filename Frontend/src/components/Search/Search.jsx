a83638cc8f9faa1215038e067cb469dde37710d8
Wheelyhog <wheelyhog@gmail.com>
2023-09-02 14:13:25 +0200

    add search form to homepage

--- Frontend/src/components/Search/Search.jsx
+++ Frontend/src/components/Search/Search.jsx
@@ -3,16 +3,21 @@ import s from './Search.module.css'
 
 export function Search({ isBlocked }) {
 
+	const handleChange = (e) => {
+		console.log(e.target.value);
+	}
+
 	return (
-		<div className={`${s.input_wrapper} ${s[isBlocked]}`} >
+		<form className={`${s.input_wrapper} ${s[isBlocked]}`} >
 			<input
 				className={s.search}
 				type='search'
 				name='search'
-				placeholder='Search in catalog' />
+				placeholder='Search in catalog' onChange={handleChange} minLength={3} />
 			<div className={s.search_icon}>
 				<img src={search} alt='search_icon' />
 			</div>
-		</div>
+			<div className={s.search_container}></div>
+		</form>
 	)
 }
