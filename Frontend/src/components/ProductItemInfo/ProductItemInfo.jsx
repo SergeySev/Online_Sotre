60f96ad0fdd926c23aab03bcc6ec77239968a6c5
Wheelyhog <wheelyhog@gmail.com>
2023-09-06 08:58:26 +0200

    finish layout product info

--- Frontend/src/components/ProductItemInfo/ProductItemInfo.jsx
+++ Frontend/src/components/ProductItemInfo/ProductItemInfo.jsx
@@ -1,30 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import s from './ProductItemInfo.module.css'
 import { Button } from '../../UI';
 import { FiHeart, FiBarChart2 } from "react-icons/fi";
-import { useSelector } from 'react-redux';
 
-export default function ProductItemInfo({ product }) {
+export default function ProductItemInfo({ product, brand_img }) {
 
     const { brand, title, madeCountry, description, price, discountPrice, inStock, mainImageLink, productImagesLinks } = product
-    const brands = useSelector(store => store.brands)
+    const [main_img, setMainImg] = useState(mainImageLink)
 
-    const brand_img = brands.filter(elem => elem.title === brand)[0].brandImageLink
-    console.log(brand_img);
 
     return (
         <div>
             <div className={s.product_info}>
                 <div className={s.product_info_made}>
-                    <img src={brand_img} alt={brand} />
+                    <img src={brand_img} alt={brand} className={s.brand_img} />
                 </div>
                 <div className={s.product_info_main}>
                     <div className={s.product_info_main_image}>
-                        <img src={mainImageLink} alt={title} className={s.product_image} />
+                        <img src={main_img} alt={title} className={s.product_image} />
                         <div className={s.product_info_rest_images}>
                             {productImagesLinks?.map((elem, index) =>
                                 <div className={s.rest_image} key={index}>
-                                    <img src={elem} alt={title} />
+                                    <img src={elem} alt={title} onClick={() => setMainImg(elem)} />
                                 </div>
                             )}
                         </div>
@@ -44,6 +41,12 @@ export default function ProductItemInfo({ product }) {
                         </div>
 
                     </div>
+                </div>
+                <div className={s.product_info_tabs}>
+                    <span className={s.product_info_tab_active}>Description</span>
+                    <span className={s.product_info_tab}>Characteristics</span>
+                    <span className={s.product_info_tab}>Delivery</span>
+                    <span className={s.product_info_tab}>Feedback</span>
                 </div>
             </div>
         </div>
