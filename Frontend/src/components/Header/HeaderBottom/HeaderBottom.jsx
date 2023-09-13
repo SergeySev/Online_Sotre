f42b3e1de42d3772682ef33f6fafabd8dbe102ca
Slava <v.zhevaga@gmail.com>
2023-09-13 21:54:03 +0200

    fix search behavior

--- Frontend/src/components/Header/HeaderBottom/HeaderBottom.jsx
+++ Frontend/src/components/Header/HeaderBottom/HeaderBottom.jsx
@@ -1,14 +1,37 @@
-import { CatalogBurger, MenuNavigation, SocialIconList, Search } from '../../'
-import s from './HeaderBottom.module.css'
+import { useEffect, useState } from "react";
+import { CatalogBurger, MenuNavigation, SocialIconList, Search } from "../../";
+import s from "./HeaderBottom.module.css";
 
 export function HeaderBottom() {
+	const [isSticky, setIsSticky] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const scrollPosition = window.scrollY;
+			if (scrollPosition > 184) {
+				setIsSticky(true);
+			} else {
+				setIsSticky(false);
+			}
+		};
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
 
 	return (
 		<div className={s.header_bottom}>
-			<CatalogBurger />
-			<MenuNavigation />
-			<SocialIconList content='header' />
-			<Search />
-		</div >
-	)
+			{!isSticky ? (
+				<>
+					<CatalogBurger />
+					<MenuNavigation />
+					<SocialIconList content="header" />
+					<Search isBlocked="bottom" />
+				</>
+			) : (
+				""
+			)}
+		</div>
+	);
 }
